ff8f7c1ad31c8254cda8cbdc9feea0f041904114
Alex Koch <lepiks@ukr.net>
2017-08-27 16:33:56 +0300

    address book

---
+++ WpfStepByStep1/Contact.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace WpfStepByStep1
+{
+	public class Contact : ViewModelEntity
+	{
+		public Contact()
+		{
+		}
+		
+		protected string name;
+		protected string email;
+		protected string phonenumber;
+		
+		public string Name
+		{
+			get {return name;}
+			set
+			{
+				if(name != value)
+				{
+					name = value;
+					NotifyPropertyChanged("Name");
+				}
+			}
+		}
+		
+		public string Email
+		{
+			get {return email;}
+			set
+			{
+				if(email != value)
+				{
+					email = value;
+					NotifyPropertyChanged("Email");
+				}
+			}
+		}
+		
+		public string PhoneNumber
+		{
+			get {return phonenumber;}
+			set
+			{
+				if(phonenumber != value)
+				{
+					phonenumber = value;
+					NotifyPropertyChanged("PhoneNumber");
+				}
+			}
+		}
+	}
+}
---
+++ WpfStepByStep1/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfStepByStep1
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			this.InitializeComponent();			
+
+			//	Add some contacts.
+			ViewModel.Contacts.Add(new Contact() {Name = "James", Email = "james@mail.com", PhoneNumber = "01234 111111"} );
+			ViewModel.Contacts.Add(new Contact() {Name = "Bob", Email = "bob@mail.com", PhoneNumber = "01234 222222"} );
+			ViewModel.Contacts.Add(new Contact() {Name = "Emma", Email = "emma@mail.com", PhoneNumber = "01234 333333"} );
+		}
+
+		private void Window_Loaded(object sender, System.Windows.RoutedEventArgs e)
+		{
+			//	Set the 'SelectedContact' of the view model to the first contact.
+			ViewModel.SelectedContact = ViewModel.Contacts[0];
+		}
+
+		private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+		{
+			//	Create a new contact.
+			Contact contact = new Contact() {Name = "New Contact"};
+			
+			//	Add it to the list.
+			ViewModel.Contacts.Add(contact);
+			
+			//	Set it as the selected contact.
+			ViewModel.SelectedContact = contact;
+		}
+	}
+}
